80f9acad013f8d8d9ab106e786af7ca53fc3b2c6
Jonsy86 <jonasnolte86@gmail.com>
2024-05-28 12:11:11 +0200

    Iteration/leere Subklassen

---
+++ com/cc/java/Honeybee.java
@@ -0,0 +1,18 @@
+package com.cc.java;
+
+public class Honeybee {
+
+
+    public String doYourJob(){
+        return "I'm a HoneyBee!";
+    }
+
+    public String fly(){
+        return "I can fly!";
+    }
+
+
+
+
+
+}
